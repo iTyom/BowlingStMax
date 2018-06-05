b004441e8c7b7ad8664c364fb2f22863736e0f62
Tom Ruscelli <tom.ruscelli@ynov.com>
2018-06-05 13:00:35 +0200

    sample ini

--- StMax/db.php
+++ StMax/db.php
@@ -1,10 +1,7 @@
 <?php
-$ini_array                   = parse_ini_file ("../sample.ini") ;
-$BDD                     = "'mysql:host=".$ini_array ["host"].";dbname=".$ini_array ["dbname"].";charset=utf8', '".$ini_array ["user"]."', '".$ini_array ["pwd"]."'";
-echo $BDD;
-     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=id5237943_bowling2;charset=utf8', 'id5237943_root', 'rootroot');
-
+  $ini_array                   = parse_ini_file ("../sample.ini") ;
+  try {
+        $bdd = new PDO(sprintf('mysql:host=%s;dbname=%s', $ini_array ['host'], $ini_array ['dbname']), $ini_array ['user'], $ini_array ['pwd']);
     }
     catch (Exception $e)
     {
